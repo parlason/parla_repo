865eeeb2a8c2b0569c3a484b34df50bdfcf50b75
PARLA son <info@parla.dk>
2013-12-22 13:00:13 +0100

    Removing Files from control

--- sites/viaartis.info.songbook-from-eldorado/files/js/wysiwyg/wysiwyg_tinymce_Vd_DVaUvcr1H2Aez20Zjw2vYf4xuFPVK36HIPJ2FZfQ.js
+++
@@ -1,1 +0,0 @@
-window.tinyMCEPreInit = {"base":"\/songbook\/sites\/all\/libraries\/tinymce\/jscripts\/tiny_mce","suffix":"","query":""};
--- sites/viaartis.info.songbook-from-eldorado/files/js/wysiwyg/wysiwyg_tinymce_xxSt4ZHbMDKvUpIQMgjuxoMmaU15caHPWmHiUAIDdKE.js
+++
@@ -1,1 +0,0 @@
-window.tinyMCEPreInit = {"base":"\/songbook-from-eldorado\/sites\/all\/libraries\/tinymce\/jscripts\/tiny_mce","suffix":"","query":""};
